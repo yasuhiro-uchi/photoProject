155cb5e538124f04ab4ff73ceba27799837615b5
yasuhiro-uchi <87101772+yasuhiro-uchi@users.noreply.github.com>
2021-07-08 13:37:56 +0900

    first-commit

---
+++ accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+# CustomUser をインポート
+from .models import CustomUser
+
+class CustomUserAdmin(admin.ModelAdmin):
+    '''管理ページのレコード一覧に表示するカラムを設定するクラス
+    '''
+    #　レコード一覧にidとusernameを表示
+    list_display = ('id', 'username')
+    # 表示するカラムにリンクを設定
+    list_display_links = ('id', 'username')
+
+# Dnango管理サイトにCustomUser、CustomUserAdminを登録する
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+
+class SignUpView(CreateView):
+    '''サインアップページのビュー
+    '''
+    # form.pyで定義したフォームのクラス
+    form_class = CustomUserCreationForm
+    #　レンダリングするテンプレート
+    template_name = "signup.html"
+    #サインアップ完了後のリダイレクト先のURLパターン
+    success_url = reverse_lazy('accounts:signup_success')
+    
+    def form_valid(self, form):
+        '''CreateViewクラスのform_valid()をオーバーライド
+            フォームのバリデーションを通過したときに呼ばれる
+            フォームデータの登録を行う
+            
+            parameters:
+                form(django.forms.Form):
+                    form_classに格納されているURLにリダイレクトさせる
+        '''
+        # form オブジェクトのフィールドの値をデータベースに保存
+        user = form.save()
+        self.object = user
+        # 戻り値はスーパークラスのform_valid()の戻り値（HttpResponseRedirect）
+        return super().form_valid(form)
+
+class SignUpSuccessView(TemplateView):
+    '''サインアップ官僚のページビュー
+    '''
+    #　レンダリングするテンプレート
+    template_name = "signup_success.html"
